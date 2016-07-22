7f30ed0d5e2a33e261f96cc2cb2feabc7e86af09
govindnidigattu <govind.nidigattu@cyient-insights.com>
2016-07-22 15:53:12 +0530

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+
+hpc <-read.table("D:\\coursera\\courses-master\\04_ExploratoryAnalysis\\household_power_consumption.txt",header=TRUE, 
+                 sep=";", na.strings = "?")
+hpc1 <-rbind(hpc[hpc$Date=="1/2/2007",],hpc[hpc$Date=="2/2/2007",])
+hpc1$Date <- as.Date(hpc1$Date,"%d/%m/%Y")
+hpc1<-cbind(hpc1, "DateTime" = as.POSIXct(paste(hpc1$Date, hpc1$Time)))
+
+png("plot2.png", width=485, height=485)
+plot(hpc1$Global_active_power ~ hpc1$DateTime, type="l", 
+     xlab= "", ylab="Global Active power (kilowatts)")
+dev.off()
