b6026fe16427f26fa7c99f11668714eafe14f466
govindnidigattu <govind.nidigattu@cyient-insights.com>
2016-07-25 11:02:59 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -4,7 +4,7 @@ hpc1 <-rbind(hpc[hpc$Date=="1/2/2007",],hpc[hpc$Date=="2/2/2007",])
 hpc1$Date <- as.Date(hpc1$Date,"%d/%m/%Y")
 hpc1<-cbind(hpc1, "DateTime" = as.POSIXct(paste(hpc1$Date, hpc1$Time)))
 
-png("plot3.png", width=485, height=485)
+png("plot3.png", width=480, height=480)
 with(hpc1, {plot(Sub_metering_1 ~ DateTime, type="l", xlab= "", 
                  ylab="Energy Sub Metering")})
 
@@ -12,4 +12,7 @@ lines(hpc1$Sub_metering_2 ~ hpc1$DateTime, col = 'Red')
 lines(hpc1$Sub_metering_3 ~ hpc1$DateTime, col = 'Blue')
 legend("topright",lty=1,lwd =3,col=c("black","red","blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+## Save to file
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
